ce04dd313c85d5f566ceeee91f1a198813f8971d
Vinesh Kannan <vinesh@omnipointment.com>
2020-12-27 19:42:32 -0600

    Generate XG time plots on the fly in server.

--- haxml/viz.py
+++ haxml/viz.py
@@ -9,6 +9,7 @@ sys.path.append("./")
 from haxml.utils import (
     get_opposing_goalpost
 )
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
 
@@ -92,3 +93,86 @@ def zoom_stadium(bounds, zoom=0.01):
     width = abs(bounds["maxX"] - bounds["minX"])
     height = abs(bounds["maxY"] - bounds["minY"])
     return zoom * width, zoom * height
+
+
+def get_xg_time_series(match):
+    """
+    Produces series for XG match time plot.
+    Args:
+        match: Inflated match data (dict).
+    Returns:
+        Tuple of (time, ags_red, ags_blue, xgs_red, xgs_blue).
+    """
+    time = []
+    ags_red = []
+    ags_blue = []
+    xgs_red = []
+    xgs_blue = []
+    ag_red = 0
+    ag_blue = 0
+    xg_red = 0
+    xg_blue = 0
+    time.append(0)
+    ags_red.append(ag_red)
+    ags_blue.append(ag_blue)
+    xgs_red.append(xg_red)
+    xgs_blue.append(xg_blue)
+    for kick in match["kicks"]:
+        if kick["type"] == "goal":
+            if kick["fromTeam"] == "red":
+                ag_red += 1
+            else:
+                ag_blue += 1
+        elif kick["type"] == "own_goal":
+            if kick["team"] == "red":
+                ag_blue += 1
+            else:
+                ag_red += 1
+        if kick["fromTeam"] == "red":
+            xg_red += kick["xg"]
+        else:
+            xg_blue += kick["xg"]
+        time.append(kick["time"])
+        ags_red.append(ag_red)
+        ags_blue.append(ag_blue)
+        xgs_red.append(xg_red)
+        xgs_blue.append(xg_blue)
+    time.append(match["score"]["time"])
+    ags_red.append(ag_red)
+    ags_blue.append(ag_blue)
+    xgs_red.append(xg_red)
+    xgs_blue.append(xg_blue)
+    return (
+        time,
+        ags_red,
+        ags_blue,
+        xgs_red,
+        xgs_blue
+    )
+
+
+def plot_xg_time_series(match):
+    """
+    Plots XG match time series for a match.
+    Args:
+        match: Inflated match data (dict).
+    Returns matplotlib Figure object.
+    """
+    fig = Figure()
+    ax = fig.add_subplot(1, 1, 1)
+    xg_time_ser = get_xg_time_series(match)
+    time, ags_red, ags_blue, xgs_red, xgs_blue = xg_time_ser
+    ax.plot(time, ags_red, color="red", linestyle="-", label="Red Actual")
+    ax.plot(time, xgs_red, color="red", linestyle="--", label="Red XG")
+    ax.plot(time, ags_blue, color="blue", linestyle="-", label="Blue Actual")
+    ax.plot(time, xgs_blue, color="blue", linestyle="--", label="Blue XG")
+    title = "Final Score: {} Won {}-{}\n{}"
+    red_score = match["score"]["red"]
+    blue_score = match["score"]["blue"]
+    winner = "Red" if red_score > blue_score else "Blue"
+    stadium_name = match["stadium"]
+    ax.set_title(title.format(winner, red_score, blue_score, stadium_name))
+    ax.set_xlabel("Match Time (secs)")
+    ax.set_ylabel("Goals")
+    ax.legend()
+    return fig
--- server/__init__.py
+++ server/__init__.py
@@ -8,7 +8,9 @@ sys.path.append("./")
 from decouple import config
 from flask import (
     Flask,
-    jsonify
+    jsonify,
+    Response,
+    send_file
 )
 from haxml.prediction import (
     generate_rows_demo,
@@ -18,7 +20,12 @@ from haxml.utils import (
     get_stadiums,
     inflate_match
 )
+from haxml.viz import (
+    plot_xg_time_series
+)
+import io
 import joblib
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import os
 import pyrebase
 
@@ -48,44 +55,96 @@ print("Loading stadiums...")
 stadiums = get_stadiums("data/stadiums.json")
 
 
-"""
-Basic hello world route to check if server is running.
-"""
-@app.route("/hello")
-def hello():
-    return "Welcome to HaxML."
-
-
-"""
-Fetch the match data for a given ID and then augment it with expected goals.
-"""
-@app.route("/xg/<mid>")
-def get_xg(mid):
-    # Fetch the packed match data from Firebase.
+def get_match_packed(mid):
+    """
+    Fetch packed match data from Firebase.
+    """
     r = db.child("match/{}".format(mid)).get()
     packed = r.val()
-    # Check if stadium data is available.
-    stadium_name = packed["stadium"]
-    if not stadium_name in stadiums:
-        return jsonify({
-            "success": False,
-            "message": "No stadium data for: {}".format(stadium_name)
-        })
-    # Inflate the packed data to get all the fields in the data schema.
-    # Predict XG with demo model and augment response with predictions.
-    match_xg = predict_xg_demo(
+    if packed is None:
+        raise ValueError("Match data not found for: {}".format(mid))
+    return packed
+
+
+def get_stadium_data(stadium_name):
+    """
+    Checks if stadium data is available.
+    """
+    if stadium_name not in stadiums:
+        raise ValueError("No stadium data for: {}".format(stadium_name))
+    return stadiums[stadium_name]
+
+
+def get_match_and_stadium(mid):
+    """
+    Helper method to get packed match data and stadium.
+    """
+    packed = get_match_packed(mid)
+    stadium = get_stadium_data(packed["stadium"])
+    return packed, stadium
+
+
+def get_match_xg(packed, stadium):
+    """
+    Predict XG with demo model and augment response with predictions.
+    """
+    return predict_xg_demo(
         inflate_match(packed),
-        stadiums[stadium_name],
+        stadium,
         generate_rows_demo,
         demo_clf
     )
-    # Return the response as JSON.
+
+
+@app.route("/hello")
+def hello():
+    """
+    Basic hello world route to check if server is running.
+    """
+    return "Welcome to HaxML."
+
+
+@app.route("/xg/<mid>")
+def get_xg(mid):
+    """
+    Fetch the match data for a given ID and then augment it with expected goals.
+    """
+    try:
+        packed, stadium = get_match_and_stadium(mid)
+    except ValueError as e:
+        return jsonify({
+            "success": False,
+            "message": str(e)
+        })
+    match_xg = get_match_xg(packed, stadium)
+    # Return inflated match data with XG as JSON.
     res = {
         "success": True,
         "mid": mid,
         "match": match_xg
     }
     return jsonify(res)
+
+
+@app.route("/xgtimeplot/<mid>.png")
+def get_xg_time_plot(mid):
+    """
+    Create and serve XG time plot for the given match.
+    """
+    try:
+        packed, stadium = get_match_and_stadium(mid)
+    except ValueError as e:
+        return jsonify({
+            "success": False,
+            "message": str(e)
+        })
+    match_xg = get_match_xg(packed, stadium)
+    # Create and save XG time plot.
+    fig = plot_xg_time_series(match_xg)
+    fig.set_size_inches(10, 6)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype="image/png")
 
 
 # Start the server on the default host.
